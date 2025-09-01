ac84ac160088fe3dd1da7c7995c0d37d9016f4a4
develop-4 <develop-4@hornet-v.com>
2025-09-01 11:35:20 +0900

    Change hamburger menu icon color to green

--- frontend/src/components/molecules/hamburger-menu.tsx
+++ frontend/src/components/molecules/hamburger-menu.tsx
@@ -68,9 +68,9 @@ export function HamburgerMenu({ onMenuItemClick, isAuthenticated = false, classN
         aria-label="メニューを開く"
       >
         {isOpen ? (
-          <X className="w-5 h-5 text-gray-600" />
+          <X className="w-5 h-5 text-green-600" />
         ) : (
-          <Menu className="w-5 h-5 text-gray-600" />
+          <Menu className="w-5 h-5 text-green-600" />
         )}
       </button>
 
