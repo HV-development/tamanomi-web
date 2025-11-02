0d0fe74ac4e28dc6cd53627224603c8746a9d2e0
YutaWatanabe-hv <yuta.watanabe@hornet-v.com>
2025-11-03 05:27:58 +0900

    fix(web): Coupon型にstatus/createdAt/updatedAtを追加

--- frontend/src/types/coupon.ts
+++ frontend/src/types/coupon.ts
@@ -5,9 +5,12 @@ export interface Coupon {
   conditions: string | null
   imageUrl: string | null
   drinkType: 'alcohol' | 'soft_drink' | 'other' | null
+  status?: string
   storeId: string
   storeName: string
   expiresAt?: Date
   uuid: string
+  createdAt?: string
+  updatedAt?: string
 }
 
