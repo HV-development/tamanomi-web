f53c2c4830af5f22062bdb8b9806b1a6c4535cde
YutaWatanabe-hv <yuta.watanabe@hornet-v.com>
2025-11-03 23:48:15 +0900

    feat: loginErrorフィールドをinitialStateに追加、@hv-development/schemasを1.44.0に更新

--- frontend/src/hooks/useAppReducer.ts
+++ frontend/src/hooks/useAppReducer.ts
@@ -16,6 +16,7 @@ export const initialState: AppState = {
     isFavoritesOpen: false,
     loginStep: "password",
     loginEmail: "",
+    loginError: null,
     signupData: null,
     emailChangeStep: "form",
     newEmail: "",
